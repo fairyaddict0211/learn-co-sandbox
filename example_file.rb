f8ac0d051292795b80465f28ec1b8413c3e03121
Nyasha Perry <nyashaperry2005@yahoo.com>
2021-03-12 20:32:59 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <h1>Dogs!</h1>
+<h3>Why Dogs are Great</h3>
+ 
+<h6>Different Breeds</h6>
+  <body>
+  <!-- this is a comment -->
+  This is the content of this page.
+  </body>
+
+</html>
